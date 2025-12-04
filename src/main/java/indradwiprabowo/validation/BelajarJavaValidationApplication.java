b70d5bd8bf61b36ce1b25c69a91f6570a8849d0d
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-04 16:17:43 +0700

    commit

---
+++ src/main/java/indradwiprabowo/validation/BelajarJavaValidationApplication.java
@@ -0,0 +1,13 @@
+package indradwiprabowo.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/indradwiprabowo/validation/BelajarJavaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package indradwiprabowo.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
