ff76815f23caeca6bdd51e7c2988d58ac5e39f76
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-10 00:17:45 +0700

    Method Validation

---
+++ src/test/java/indradwiprabowo/validation/MethodValidationTest.java
@@ -0,0 +1,44 @@
+package indradwiprabowo.validation;
+
+import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.Set;
+
+public class MethodValidationTest extends AbstractValidatorTest {
+
+    @Test
+    void testSayHello() throws NoSuchMethodException {
+        Person person = new Person();
+        String name = "";
+        Method method = Person.class.getMethod("sayHello", String.class);
+
+        Set<ConstraintViolation<Person>> violations = executableValidator.validateParameters(person, method, new Object[]{name});
+        for (ConstraintViolation<Person> violation : violations) {
+            System.out.println(violation.getPropertyPath());
+            System.out.println(violation.getMessage());
+        }
+    }
+
+    @Test
+    void testSayHelloReturnValue() throws NoSuchMethodException {
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("");
+
+        String returnValue = person.fullName();
+
+        Method method = Person.class.getMethod("fullName");
+
+        Set<ConstraintViolation<Person>> violations = executableValidator
+                .validateReturnValue(person, method, returnValue);
+
+        for (ConstraintViolation<Person> violation : violations) {
+            System.out.println(violation.getPropertyPath());
+            System.out.println(violation.getMessage());
+            System.out.println("====================");
+        }
+    }
+
+}
