3ffc0390c5ec33bb692cfa2ae9ad6315467dab4d
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-13 10:10:14 +0700

    Constraint Validator Context

--- src/main/java/indradwiprabowo/validation/constraint/CheckPasswordValidator.java
+++ src/main/java/indradwiprabowo/validation/constraint/CheckPasswordValidator.java
@@ -6,13 +6,34 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class CheckPasswordValidator implements ConstraintValidator<CheckPassword, Register> {
 
+    private String messageTemplate;
+
+    @Override
+    public void initialize(CheckPassword constraintAnnotation) {
+        messageTemplate = constraintAnnotation.message();
+    }
+
     @Override
     public boolean isValid(Register value, ConstraintValidatorContext context) {
         if (value.getPassword() == null || value.getRetypePassword() == null) {
             return true; // skip validation
         }
 
-        return value.getPassword().equals(value.getRetypePassword());
+        boolean isValid = value.getPassword().equals(value.getRetypePassword());
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(messageTemplate)
+                    .addPropertyNode("password")
+                    .addConstraintViolation();
+
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(messageTemplate)
+                    .addPropertyNode("retypePassword")
+                    .addConstraintViolation();
+        }
+
+        return isValid;
     }
 
 }
