c42c7f303bf210d25bed8ca5ae1a399ac2069564
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-13 12:05:14 +0700

    Meta Data

---
+++ src/test/java/indradwiprabowo/validation/MetadataTest.java
@@ -0,0 +1,22 @@
+package indradwiprabowo.validation;
+
+import jakarta.validation.metadata.BeanDescriptor;
+import jakarta.validation.metadata.ConstraintDescriptor;
+import jakarta.validation.metadata.PropertyDescriptor;
+import org.junit.jupiter.api.Test;
+
+public class MetadataTest extends AbstractValidatorTest {
+
+    @Test
+    void testMetadata() {
+        BeanDescriptor beanDescriptor = validator.getConstraintsForClass(Person.class);
+
+        for (PropertyDescriptor constrainedProperty : beanDescriptor.getConstrainedProperties()) {
+            System.out.println(constrainedProperty.getPropertyName());
+            for (ConstraintDescriptor<?> constraintDescriptor : constrainedProperty.getConstraintDescriptors()) {
+                System.out.println(constraintDescriptor);
+            }
+        }
+    }
+
+}
